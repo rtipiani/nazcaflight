2bc9f27517b6fee8f19dc097c3b6d58256fcb30e
Rubén Tipiani <rtipiani@gmail.com>
2025-04-26 20:33:31 -0500

    Project final

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://nazcaflightandtours.com',
     vite: {
         plugins: [tailwindcss()],
     }
